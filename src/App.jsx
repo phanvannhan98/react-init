c3af63ae4d40b17543261111e7c84bb422d99f28
phanvannhan98 <phanvannhan98@gmail.com>
2024-08-01 23:02:11 +0700

    init react project

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+function App() {
+  const initPlayers = [
+    { id: 1, name: 'A', score: 1 },
+    { id: 2, name: 'B', score: 1 },
+  ];
+
+  const [count, setCount] = useState(0);
+
+  const [players, setPlayers] = useState([...initPlayers]);
+
+  const reset = () => {
+    setPlayers([...initPlayers]);
+  };
+
+  const addPlayer = () => {
+    if (players.length >= 5) return;
+
+    const id = players.length + 1;
+    const name = String.fromCharCode(65 + players.length);
+    const newPlayer = { id, name, score: 1 };
+
+    setPlayers((oldPlayers) => [...oldPlayers, newPlayer]);
+  };
+
+  const addScoreRandom = () => {};
+
+  return (
+    <>
+      <button onClick={addPlayer}>add</button>
+      <button onClick={reset}>reset</button>
+
+      <div className="flex flex-row">
+        {players.map(({ name, score }) => (
+          <div className="flex flex-col gap-2">
+            <div>{name}</div>
+            <div>{score}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
